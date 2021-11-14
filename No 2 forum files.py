c60c986b3e3fbaa79c2c9a6cbaf9dfcfb27006ae
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-11-14 23:55:52 +0700

    Update No 2 forum files.py

--- No 2 forum files.py
+++ No 2 forum files.py
@@ -1,7 +1,15 @@
-file =open("myVacation.txt","r")
-myList = file.readlines()
+file =open("myVacation.txt","r") #open the text file which contain original text
+myList = file.readlines() #output will turn into a list
 
-count= 0
-for line in myList:
-    count= count + 1
-    print(count,".", line,sep="",end="")
+count = 1 #start from number 1
+newList=[] # add empty list 
+for line in myList: #start looping
+    count = count + 1 #add +1 in new line for looping
+    formula = "{} {}".format(count,line) 
+    newList.append(formula) #append formula to list
+
+newListIntoStr= " ".join([str(list)for list in newList]) #transform list into string, because newFile.write doesnt accept list
+
+newFile=open("myVacationResult.txt","w") #open new blank text file to write
+newFile.write(newListIntoStr) #write the final result in newfile
+file.close() 
